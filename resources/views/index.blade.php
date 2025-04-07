9c14e465640a74f07d929771a5b8f34cac22d913
Fuad Hasanli <fhesenli92@gmail.com>
2025-04-07 18:19:53 +0400

    front

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <title>Healthy Product - Sağlıklı Yaşam İçin Ürün Analizi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <!-- Navbar -->
@@ -209,4 +209,4 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html> 
+</html>

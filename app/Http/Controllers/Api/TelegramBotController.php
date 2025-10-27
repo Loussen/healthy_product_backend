10e635f444167c0ad0c8c0b99cd8d57641cccd7c
Fuad Hasanli <fhesenli92@gmail.com>
2025-10-27 18:16:54 +0400

    Update TelegramBotController.php

--- app/Http/Controllers/Api/TelegramBotController.php
+++ app/Http/Controllers/Api/TelegramBotController.php
@@ -9,25 +9,10 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramBotController extends BaseController
 {
-    /**
-     * @throws TelegramSDKException
-     */
-    public function handleWebhook(Request $request)
+    public function test()
     {
-        $telegram = new Api(config('telegram.bots.mybot.token'));
+        $updates = Telegram::getUpdates();
 
-        $update = $telegram->getWebhookUpdate();
-        $message = $update->getMessage();
-        $text = $message->getText();
-        $chatId = $message->getChat()->getId();
-
-        if ($text === '/start') {
-            $telegram->sendMessage([
-                'chat_id' => $chatId,
-                'text' => "👋 Salam! Bot-a xoş gəldin.\nBuradan başlayırıq!"
-            ]);
-        }
-
-        return response('ok');
+        dd($updates);
     }
 }

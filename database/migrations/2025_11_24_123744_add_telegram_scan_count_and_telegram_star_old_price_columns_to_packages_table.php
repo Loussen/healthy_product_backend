c26b21e2cc89ba952b7742c4357ee2b579ae956f
Fuad Hasanli <fhesenli92@gmail.com>
2025-11-24 12:39:08 +0400

    Create 2025_11_24_123744_add_telegram_scan_count_and_telegram_star_old_price_columns_to_packages_table.php

---
+++ database/migrations/2025_11_24_123744_add_telegram_scan_count_and_telegram_star_old_price_columns_to_packages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->integer('telegram_scan_count')->nullable();
+            $table->double('telegram_star_old_price')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->dropColumn('telegram_scan_count');
+            $table->dropColumn('telegram_star_old_price');
+        });
+    }
+};
